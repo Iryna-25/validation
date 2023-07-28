67e44cd8e4e411f93361d0e0809cffe7e18ac0f8
Iryna Shandar <iryna.shandar@gmail.com>
2023-07-28 15:17:00 +0300

    validation

--- script.js
+++ script.js
@@ -1,35 +1,30 @@
-const form = document.querySelector('#login-form');
-const pass_reg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+const emailField = document.getElementById("emailField");
+const passwordField = document.getElementById("paswordField");
+const button = document.getElementById("button");
+const responseEmail = document.getElementById("responseEmail");
+const responsePassword = document.getElementById("responsePassword");
 
-let username = form.elements.namedItem("username");
-let password = form.elements.namedItem("password");
-
-username.addEventListener('input', validate);
-password.addEventListener('input', validate);
-
-form.addEventListener('submit', function (e) {
- e.preventDefault();
- return true;
+button.addEventListener("click", function () {
+  let valid = true;
+  const email = emailField.value;
+  const password = paswordField.value;
+  if (!validateEmail(email)) {
+    responseEmail.innerHTML = "Sorry, this email is not cool enough 😩";
+    valid = false;
+  }
+  if (!validateTel(tel)) {
+    responsePassword.innerHTML = "Sorry, this tel is not cool enough 😩";
+    valid = false;
+  }
+  return valid;
 });
 
-function validate (e) {
- if (e.target.name == "password") {
-  if (pass_reg.test(e.target.value)) {
-   e.target.classList.add('valid');
-   e.target.classList.remove('invalid');
-  } else {
-   e.target.classList.add('invalid');
-   e.target.classList.remove('valid');
-  }
- }
- 
- if (e.target.name == "username") {
-  if (e.target.value.length > 3) {
-   e.target.classList.add('valid');
-   e.target.classList.remove('invalid');
-  } else {
-   e.target.classList.add('invalid');
-   e.target.classList.remove('valid');
-  }
- }
+function validateEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+function validatePassword(password) {
+  const re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+  return re.test(String(password));
 }
